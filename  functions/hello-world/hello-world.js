f8652dff4136aae159d02e8dd94313e8e69d7b4c
Joseph Bacal <bacalj@gmail.com>
2021-11-11 07:17:09 -0500

    simplifying futher

---  functions/hello-world/hello-world.js
+++  functions/hello-world/hello-world.js
@@ -1,8 +1,5 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello World` }),
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let resultsArea = document.querySelector('#results')
+let helloendpoint = 'https://mini-sane.netlify.app/.netlify/functions/hello-world'
+
 async function pingTheFunction(){
-    const result = await axios.get('https://mini-sane.netlify.app/.netlify/functions/hello-world')
-    .then((r) => {
+    await axios.get(helloendpoint).then((r) => {
         console.log(r)
-        document.querySelector('#results').innerHTML = r.data.message;
-    }).catch((e)=> console.log(e))
+        resultsArea.innerHTML = r.data.message;
+    })
+    .catch((e)=> console.log(e))
 }

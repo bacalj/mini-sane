4b267f8c943c2052108cd9f6de17d05abf641054
Joseph Bacal <bacalj@gmail.com>
2021-11-11 18:11:56 -0500

    foof

---  functions/hello-world/hello-world.js
+++  functions/hello-world/hello-world.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 exports.handler = async (event, context) => {
-  //await axios.get('https://icanhazdadjoke.com', { headers: { "Accept": "text/plain" } })
+  let bob = await axios.get('https://icanhazdadjoke.com', { headers: { "Accept": "text/plain" } })
   return {
     statusCode: 200,
     body: "Hello, World"

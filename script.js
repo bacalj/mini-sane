5b12c04131f99907f690eb965816369b3a77cec0
Joseph Bacal <bacalj@gmail.com>
2021-11-11 07:22:08 -0500

    we see

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var resultsArea = document.querySelector('#results')
 let helloendpoint = 'https://mini-sane.netlify.app/.netlify/functions/hello-world'
 
 async function pingTheFunction(){
     await axios.get(helloendpoint).then((r) => {
         console.log(r)
-        resultsArea.innerHTML = r.data.message;
+        document.querySelector('#results').innerHTML = r.data.message;
     })
     .catch((e)=> console.log(e))
 }

3b7c5fc058a59209153e6b3bf4fb19a5ea4e974f
Joseph Bacal <bacalj@gmail.com>
2021-11-11 17:48:15 -0500

    okaaaaay

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let helloendpoint = 'https://mini-sane.netlify.app/.netlify/functions/hello-world'
 
-async function helloWorld(){
+async function getFromFunction(){
     await axios.get(helloendpoint).then((r) => {
         console.log(r)
         document.querySelector('#results').innerHTML = r.data;
@@ -11,4 +11,5 @@ async function helloWorld(){
 async function clientGetJoke(){
     await axios.get('https://icanhazdadjoke.com', { headers: { "Accept": "text/plain" } })
     .then((r) => console.log(r))
+    document.querySelector('#results').innerHTML = r.data;
 }

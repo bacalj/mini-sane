57c20ecc5b6dfa1dbfa838171675a711aa90dea1
Joseph Bacal <bacalj@gmail.com>
2021-11-11 18:15:46 -0500

    is it axios

---  functions/hello-world/hello-world.js
+++  functions/hello-world/hello-world.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 exports.handler = async (event, context) => {
-  let bob = await axios.get('https://icanhazdadjoke.com', { headers: { "Accept": "text/plain" } })
+  console.log(axios)
   return {
     statusCode: 200,
     body: "Hello, World"

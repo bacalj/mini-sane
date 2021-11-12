7fc79860861acf10eb6b9be6252584307cf1988d
Joseph Bacal <bacalj@gmail.com>
2021-11-11 19:56:55 -0500

    maybe

---  functions/hello-world/hello-world.js
+++  functions/hello-world/hello-world.js
@@ -1,11 +1,41 @@
-const { default: axios } = require("axios");
+// const { default: axios } = require("axios");
+
+// exports.handler = async (event, context) => {
+//   axios.get('icanhazdadjoke').then((r) => {
+//     console.log(r)
+//   })
+//   return {
+//     statusCode: 200,
+//     body: "Hello, World"
+//   };
+// };
+
+var axios = require("axios").default;
 
 exports.handler = async (event, context) => {
-  axios.get('icanhazdadjoke').then((r) => {
-    console.log(r)
-  })
-  return {
-    statusCode: 200,
-    body: "Hello, World"
+    let mot = event.queryStringParameters.word
+
+    var options = {
+        url: `https://pokeapi.co/api/v2/pokemon/ditto`,
+    };
+
+    let data
+
+    try {
+     data = await axios.request(options).then(response => response.data )
+    } catch (e) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: e.message
+        })
+      }
+    }
+
+
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
   };
-};

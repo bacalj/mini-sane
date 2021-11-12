d38030012e909f9a6ac9de2ea74a90ef49661ae5
Joseph Bacal <bacalj@gmail.com>
2021-11-11 19:50:53 -0500

    will rhia qork?

---  functions/hello-world/hello-world.js
+++  functions/hello-world/hello-world.js
@@ -1,7 +1,9 @@
 const { default: axios } = require("axios");
 
 exports.handler = async (event, context) => {
-  axios.get('icanhazdadjoke')
+  axios.get('icanhazdadjoke').then((r) => {
+    console.log(r)
+  })
   return {
     statusCode: 200,
     body: "Hello, World"
